954c305b3d81c1215bf26f5425b378c9f1637a5e
vcsil <vcsilva@poli.ufrj.br>
2022-10-07 06:22:32 -0300

    :sparkles: feat: connect DOM

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <h1>Oii</h1>
+        </BrowserRouter>
+    );
+}
+
+export default App;
